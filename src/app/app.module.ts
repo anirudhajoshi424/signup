e61ad1cf241c95636404d1e3dd1c7bf5e3bafcaa
anirudhajoshi424 <anirudhajoshi424@gmail.comgit config --global user.email anirudhajoshi424@gmail.comgit config --global user.email anirudhajoshi424@gmail.com>
2018-05-01 21:45:04 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { LinkComponent } from './link/link.component';
+import { ExitComponent } from './exit/exit.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    LinkComponent,
+    ExitComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'link', pathMatch: 'full'},
+      { path: 'link', component: LinkComponent},
+      { path: 'signin', component: SigninComponent},
+      { path: 'exit', component: ExitComponent}
+    ])
   ],
+
+  
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/link/link.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-link',
+  templateUrl: './link.component.html',
+  styleUrls: ['./link.component.css']
+})
+export class LinkComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+  goTosignin(){
+    this.router.navigate(['/signin']);
+  }
+}
